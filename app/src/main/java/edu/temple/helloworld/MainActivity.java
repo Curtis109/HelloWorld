9c490d1a2f0e7a8c556a32e8e9b34794d2f46de4
Curtis Kieser <tuh39868@temple.edu>
2020-02-06 04:01:20 -0500

    Add files via upload

---
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -0,0 +1,20 @@
+package edu.temple.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void buttonClick(View v){
+        TextView tv = (TextView)findViewById(R.id.textView);
+        tv.setText("You have clicked the button");
+    }
+}
